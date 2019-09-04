759dbbebbeb26d896c0178a79f524970aead9ee4
FrankYeah <aaa24295234@gmail.com>
2019-09-05 00:35:39 +0800

    o

--- js/index.js
+++ js/index.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
         autoplayTimeout:4000,
         lazyLoad: true
     });
-  });
+
+
+    var menuOpen = document.getElementById("menuOpen");
+    var menuClose = document.getElementById("menuClose");
+    var menu = document.getElementById("menu");
+
+    menuOpen.addEventListener("click", function(){
+        $(menu).css('display', 'inline');
+    });
+
+    menuClose.addEventListener("click", function(){
+        $(menu).css('display', 'none');
+    });
+
+});

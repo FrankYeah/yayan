6d20e5014ada7b5dc2fbf9961d61a38bba5e91ef
FrankYeah <aaa24295234@gmail.com>
2019-09-06 10:19:13 +0800

    p

---
+++ js/portfolio.js
@@ -0,0 +1,29 @@
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+var item5 = document.getElementById("item5");
+item1.addEventListener('mouseover', over)
+item2.addEventListener('mouseover', over)
+item3.addEventListener('mouseover', over)
+item4.addEventListener('mouseover', over)
+item5.addEventListener('mouseover', over)
+item1.addEventListener('mouseleave', leave)
+item2.addEventListener('mouseleave', leave)
+item3.addEventListener('mouseleave', leave)
+item4.addEventListener('mouseleave', leave)
+item5.addEventListener('mouseleave', leave)
+
+function over () {
+    this.children[0].style.transform = 'scale(1.1)';
+    this.children[0].style.opacity = '0.6';
+    this.children[1].style.height = '60px';
+    this.style.color = '#c59b6d';
+}
+
+function leave () {
+    this.children[0].style.transform = 'scale(1)';
+    this.children[0].style.opacity = '1';
+    this.children[1].style.height = '50px';
+    this.style.color = '#604a40';
+}

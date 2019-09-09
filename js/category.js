974f5c68e81cf5917732c9ba8d072d9a3386dd64
FrankYeah <aaa24295234@gmail.com>
2019-09-09 18:01:26 +0800

    carousel

--- js/category.js
+++ js/category.js
@@ -1,25 +1,14 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        items: 3,
-        loop: true,
-        // merge: true,
-        // margin: 10,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoWidth: true,
-        // URLhashListener: true,
-        // autoplayHoverPause: true,
-        // startPosition: 'URLHash',
-        // dotsContainer: '.custom-dots',
-        // responsiveRefreshRate: 0,
-        responsive: {
-          0: {
-            autoWidth: false
-          },
-          992: {
-            autoWidth: false
-          }
-        }
-      });
+
+
+
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 
 })

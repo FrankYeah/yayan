1f71c72f2bae93a3d1150ebe7ff5a94d6794abae
FrankYeah <aaa24295234@gmail.com>
2019-09-11 17:59:50 +0800

    big pic

--- js/content.js
+++ js/content.js
@@ -1,76 +1,75 @@
 $(document).ready(function(){
 
+
+
     var swiper = new Swiper('.swiper-container', {
-      lazy: true,
       loop: true,
-      slidesPerView: 1,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+      lazy: true,
+      autoplay: 1000,
+      slidesPerView: 1
     });
 
+
     var fullBox = document.getElementById("fullBox");
-    var fullInner = document.getElementById("fullInner");
     var leftArrow = document.getElementById("leftArrow");
     var rightArrow = document.getElementById("rightArrow");
+    var swiperContainer = document.getElementById('swiperContainer');
     var bg1 = document.getElementById("bg1");
     var bg2 = document.getElementById("bg2");
     var bg3 = document.getElementById("bg3");
     var bg4 = document.getElementById("bg4");
     var bg5 = document.getElementById("bg5");
     var bg5 = document.getElementById("bg5");
-    fullBox.addEventListener('click', function(event){
-        $(fullBox).css('display', 'none');
+
+
+    fullBox.addEventListener('click', function(){
+        $(fullBox).css('visibility', 'hidden');
     })
 
-    fullInner.addEventListener('click', function(event){
+    swiperContainer.addEventListener('click', function(event){
         event.stopPropagation();
     })
 
     leftArrow.addEventListener('click', function(event){
+        swiper.slidePrev()
         event.stopPropagation();
-        swiper.slidePrev(600)
+
     })
 
     rightArrow.addEventListener('click', function(event){
+        swiper.slideNext()
         event.stopPropagation();
-        swiper.slideNext(600)
     })
 
     bg1.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(1);
     })
 
     bg2.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(2);
     })
 
     bg3.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(3);
     })
 
     bg4.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(4);
     })
     
     bg5.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(5);
     })
 
     bg6.addEventListener('click', function(){
-        $(fullBox).css('display', 'flex');
+        $(fullBox).css('visibility', 'visible');
         swiper.slideTo(6);
     })
-
-
-
 
 
   })

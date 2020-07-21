38e1e0bf207ca235b3a47f2385114244d9c23e48
FrankYeah <aaa24295234@gmail.com>
2020-07-21 13:24:10 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -10,16 +10,16 @@ $(document).ready(function(){
     });
 
 
-    var menuOpen = document.getElementById("menuOpen");
-    var menuClose = document.getElementById("menuClose");
-    var menu = document.getElementById("menu");
+    // var menuOpen = document.getElementById("menuOpen");
+    // var menuClose = document.getElementById("menuClose");
+    // var menu = document.getElementById("menu");
 
-    menuOpen.addEventListener("click", function(){
-        $(menu).css('display', 'inline');
-    });
+    // menuOpen.addEventListener("click", function(){
+    //     $(menu).css('display', 'inline');
+    // });
 
-    menuClose.addEventListener("click", function(){
-        $(menu).css('display', 'none');
-    });
+    // menuClose.addEventListener("click", function(){
+    //     $(menu).css('display', 'none');
+    // });
 
 });

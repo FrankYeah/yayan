9d35296e0ecb2db6b4a93d428debab83566add25
Murphy Wu <murphy.wu@core-code.tech>
2023-07-07 17:49:27 +0800

    add line url

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
-$(document).ready(function () {
-  window.onload = function () {
-    var owl = $('.owl-carousel').owlCarousel({
-      loop: true,
-      items: 1,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      // lazyLoad: true,
-      // animateIn: 'fadeIn',
-      // animateOut: 'fadeOut',
-      // animateIn: 'fadeLeftInLeft',
-    });
-    owl.trigger('to.owl.carousel', [1]);
-  };
+document.addEventListener('DOMContentLoaded', function () {
+  // 检查是否已经添加过参数
+  var hasAddedParameter = false;
+
+  // 检查网址是否已经包含参数
+  if (window.location.search.indexOf('openExternalBrowser=1') !== -1) {
+    hasAddedParameter = true;
+  }
+
+  // 如果没有添加过参数，则添加参数
+  if (!hasAddedParameter) {
+    const url = window.location.href;
+    const modifiedUrl = url + '?openExternalBrowser=1';
+    window.location.href = modifiedUrl;
+  }
+
+  var owl = $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    // lazyLoad: true,
+    // animateIn: 'fadeIn',
+    // animateOut: 'fadeOut',
+    // animateIn: 'fadeLeftInLeft',
+  });
+  owl.trigger('to.owl.carousel', [1]);
 
   // var menuOpen = document.getElementById("menuOpen");
   // var menuClose = document.getElementById("menuClose");
